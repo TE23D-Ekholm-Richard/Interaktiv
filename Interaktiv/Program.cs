782d740a25c2339fa1fb687c5ae557cf28bd5193
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-08-30 10:47:53 +0200

    dcdcc

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿Console.WriteLine("Användarnamn: ");
+string name = Console.ReadLine();
+
+if (name == "Sigma");
+{
+    Console.WriteLine("Välkommen mitt barn!");
+}
+else
+{
+    Console.WriteLine("Nej mitt barn jag är besviken...");
+
+}
+
+
+Console.ReadLine();
